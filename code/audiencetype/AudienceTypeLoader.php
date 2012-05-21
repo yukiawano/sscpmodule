356171641e29218492e0d9ecb97ae05bc3ecef0c
Yuki Awano <y.a.unix@gmail.com>
2012-05-22 01:01:11 +0900

    Removed unused codes

--- code/audiencetype/AudienceTypeLoader.php
+++ code/audiencetype/AudienceTypeLoader.php
@@ -6,13 +6,7 @@
  */
 class AudienceTypeLoader{
 	public function load(){
-		// require_once 'thirdparty/zend_translate_railsyaml/library/Translate/Adapter/thirdparty/sfYaml/lib/sfYamlParser.php';
-		// $parser = new sfYamlParser();
-		// $result = $parser->parse(file_get_contents(SSCP_PATH.'/_config/audiencetype.yml'));
-		
 		$result = Config::inst()->get("AudienceType", "AudienceTypes");
-		var_dump($result);
 		return $result;
-		// return $result["AudienceType"]["AudienceTypes"];
 	}
 }
