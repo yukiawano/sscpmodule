fe46ab517f544b3b9b422cf1535d616f69841aca
Yuki Awano <y.a.unix@gmail.com>
2012-06-01 01:31:24 +0900

    MINOR Commit for a discussion.

--- code/controllers/BlockHolderMain.php
+++ code/controllers/BlockHolderMain.php
@@ -1,45 +1,57 @@
 <?php
-class BlockHolderMain extends LeftAndMain {
+/**
+ * @package sscp
+ */
+class BlockHolderMain extends CMSSettingsController {
+// class BlockHolderMain extends LeftAndMain {
     static $url_segment = 'personalization';
-    static $url_rule = '$Action/$ID';
+    static $url_rule = '/$Action/$ID';
     static $menu_title = 'Personalization';
-    static $menu_priority = 60;
-	static $tree_class = "BlockHolder";
-    static $allowed_actions = array('EditForm', 'createholder');
-	
+    static $menu_priority = -1;
+    
+	/*
     public function init(){
     	parent::init();
     	Requirements::javascript(CMS_DIR . '/javascript/CMSMain.GridField.js');
 		Requirements::css(CMS_DIR . '/css/screen.css');
-    }
+    }*/
     
-    public function getEditForm($id = null, $fields = null){
+    function getEditForm($id = null, $fields = null){
+    	return new Form($this, "EditForm", new FieldList(new LabelField("Foo", "Bar")), new FieldList());
+    }
+    	/*
     	$fields = new FieldList();
-    	$config = GridFieldConfig::create();
-    	$config->addComponent(new GridFieldDeleteAction());
-    	$config->addComponent(new GridFieldDataColumns());
-    	$config->addComponent(new GridFieldSortableHeader());
-    	$config->addComponent(new GridFieldEditButton());
-    	$config->addComponent(new GridFieldDetailForm());
-    	$config->addComponent(new GridFieldAddNewButton());
+    	$config = GridFieldConfig::create()->addComponents(
+				new GridFieldToolbarHeader(),
+				new GridFieldSortableHeader(),
+				new GridFieldDataColumns(),
+				new GridFieldPaginator(),
+    			new GridFieldDeleteAction(),
+    			new GridFieldEditButton(),
+    			new GridFieldDetailForm(),
+    			new GridFieldAddNewButton());
     	$gridField = new GridField('BlockHolders', null, BlockHolder::get(), $config);
     	
-    	$actions = new FieldList();
-    	$actions->push(new FormAction("createholder","New BlockHolder"));
-    			
-    	$form = new Form($this, "EditForm",
-    				new FieldList($gridField), new FieldList());
+    	$fields = new FieldList(
+    			new TabSet('Root',
+    				$blockHolderTab = new Tab('Main',$gridField),
+    				$snippetTab = new Tab('Access')));
+    	$form = new Form($this, "EditForm", $fields, new FieldList());
+    	
 		$form->addExtraClass('root-form');
-    	$form->addExtraClass('cms-edit-form cms-panel-padded center');
-    	
-    	// $form->setTemplate('LeftAndMain_EditForm');
-    	// $form->addExtraClass('cms-content cms-edit-form center ss-tabset');
-    	// if($form->Fields()->hasTabset()) $form->Fields()->findOrMakeTab('Root')->setTemplate('CMSTabSet');
-    	
-		//$this->extend('updateEditForm', $form);
+		$form->addExtraClass('cms-edit-form center'); // cms-panel-padded 
+		if($form->Fields()->hasTabset()) $form->Fields()->findOrMakeTab('Root')->setTemplate('CMSTabSet');
+		$form->setHTMLID('Form_EditForm');
+		$form->setTemplate($this->getTemplatesWithSuffix('_EditForm'));
+		
+    	// $form->setTemplate($this->getTemplatesWithSuffix('_EditForm'));
+		// $form->addExtraClass('cms-edit-form center ' . $this->BaseCSSClasses());
+		
+		 $this->extend('updateEditForm', $fields);
     	return $form;
-    }
-    
+    }*/
+    	
+    /*
     public function createholder(){
     	$blockHolder = new BlockHolder();
     	$blockHolder->Name = "Sample".time();
@@ -49,4 +61,23 @@ class BlockHolderMain extends LeftAndMain {
     	echo '<script type="text/javascript">statusMessage("'.$blockHolder->Name.' is created.");</script>';
 		return $this->getResponseNegotiator()->respond($this->request);
     }
+    
+    function LinkPreview() {
+    	return false;
+    }*/
+    /*
+    function LinkPreview() {
+    	return false;
+    }*/
+    /*
+    function Breadcrumbs($unlinked = false) {
+    	return new ArrayList(array(
+    			new ArrayData(array(
+    					'Title' => $this->SectionTitle(),
+    					'Link' => false
+    			))
+    	));
+    }*/
+    
+    
 }
