b7bc1f45237a9a1c30f1428322cb0e820953e21d
Yuki Awano <y.a.unix@gmail.com>
2012-06-07 21:39:41 +0900

    BUGFIX Fixed a test in testLoad in AudienceTypeLoaderTest has not passed.

--- tests/audiencetype/AudienceTypeLoaderTest.php
+++ tests/audiencetype/AudienceTypeLoaderTest.php
@@ -6,13 +6,15 @@
 
 class AudienceTypeLoaderTest extends SapphireTest{
 	function testLoad(){
+		// You may need to rename mysite/_config/audiencetype.yml not to load the configuration.
+		
 		$audienceTypeLoader = new AudienceTypeLoader();
 		$result = $audienceTypeLoader->load();
 		$expected = array("InclusiveOR" => array(
 				"NewComer" => array("NewComer" => true),
-				"NewYoker" => array(
+				"NewYorker" => array(
 						"Location" => "NewYork",
-						"OS" => "iOS")
+						"Device" => "Linux")
 				));
 		$this->assertEquals($result,$expected);
 	}
