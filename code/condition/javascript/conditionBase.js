4d752b9f69162d24d1e319895635e8bab9847b1c
Yuki Awano <y.a.unix@gmail.com>
2012-06-22 02:02:16 +0900

    MINOR Removed unsed alert functions and refactored the code.

--- code/condition/javascript/conditionBase.js
+++ code/condition/javascript/conditionBase.js
@@ -2,15 +2,17 @@
  * ConditionBase.js provides functions for setting result of condition.
  */
 
-// Getting location by using html5 geo-location api
+// Get location by using html5 geo-location api and reverse-geo-coding api by OpenStreetMap.
+// https://wiki.openstreetmap.org/wiki/Nominatim
 
 var CPEnvLocationKey = 'CPEnvLocationJ';
+var SourceIsHTML5 = 'HTML5-GeolocationAPI';
 
 (function($) {
 
 function locationSourceIsHTML5() {
 	var address = JSON.parse($.cookie(CPEnvLocationKey));
-	if(address != null && address.Source == 'HTML5-GeolocationAPI') {
+	if(address != null && address.Source == SourceIsHTML5) {
 		return true;
 	} else {
 		return false;
@@ -18,35 +20,35 @@ function locationSourceIsHTML5() {
 }
 
 if(!locationSourceIsHTML5() && navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-			alert("Your latitude is " + position.coords.latitude + " and longitude is " + position.coords.longitude);
-			
-			$.getJSON("http://nominatim.openstreetmap.org/reverse",
-					{format: "json",
-				lat: position.coords.latitude,
-				lon: position.coords.longitude,
-				zoom: 18,
-				'accept-language': 'en'},
-				function(json){
-					var detailedAddress = json.address;
-					var address = $.parseJSON($.cookie(CPEnvLocationKey));
-					if(address == null) address = {};
+	navigator.geolocation.getCurrentPosition(
+		function(position) {
+			$.getJSON( "http://nominatim.openstreetmap.org/reverse",
+					   { format: "json",
+				         lat: position.coords.latitude,
+				         lon: position.coords.longitude,
+				         zoom: 18,
+				         'accept-language': 'en' },
+				       function(json){
+				           var detailedAddress = json.address;
+				           
+				           var address = $.parseJSON($.cookie(CPEnvLocationKey));
+				           if(address == null) address = {};
 					
-					alert(JSON.stringify(address));
+				           address.Latitude = json.lat;
+				           address.Longitude = json.lon;
+				           address.Country = detailedAddress.country;
+				           address.Region = detailedAddress.state + ' ' + detailedAddress.region;
+				           address.City = detailedAddress.city;
+				           address.County = detailedAddress.county;
+				           address.Road = detailedAddress.road;
+				           address.PublicBuilding = detailedAddress.public_building;
+				           address.Postcode = detailedAddress.postcode;
+				           address.Source = SourceIsHTML5';
 					
-					address.Latitude = json.lat;
-					address.Longitude = json.lon;
-					address.Country = detailedAddress.country;
-					address.Region = detailedAddress.state + ' ' + detailedAddress.region;
-					address.City = detailedAddress.city;
-					address.County = detailedAddress.county;
-					address.Road = detailedAddress.road;
-					address.PublicBuilding = detailedAddress.public_building;
-					address.Postcode = detailedAddress.postcode;
-					address.Source = 'HTML5-GeolocationAPI';
-					
-					$.cookie(CPEnvLocationKey,JSON.stringify(address));
-				});
+				           $.cookie(CPEnvLocationKey,JSON.stringify(address));
+				       }
+				     );
 	});
 }
+
 })(jQuery);
