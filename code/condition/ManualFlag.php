8c57a60f757516306e587a2c79d0eaacd103843c
Yuki Awano <y.a.unix@gmail.com>
2012-07-01 18:16:11 +0900

    ENHANCEMENT Added ManualFlag condition. (Remaining Tasks, Add tests and documents)

---
+++ code/condition/ManualFlag.php
@@ -0,0 +1,18 @@
+<?php
+class ManualFlag extends ConditionBase {
+	
+	const ManualFlagPrefix = 'MF';
+	
+	function doesSatisfy(CPEnvironment $env, $args) {
+		$flagName = $args;
+		
+		$result = $env->get("{self::ManualFlagPrefix}_{$flagName}", false);
+		
+		if($result) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+}
