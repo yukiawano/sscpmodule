707f81437e31095bc7bfd9e3b4f4f2b0bcc7a491
Yuki Awano <y.a.unix@gmail.com>
2012-07-31 02:23:56 +0900

    MINOR Refactored existing javascript codes

--- code/condition/javascript/conditionBase.js
+++ code/condition/javascript/conditionBase.js
@@ -4,59 +4,92 @@
 
 // Get location by using html5 geo-location api and reverse-geo-coding api by OpenStreetMap.
 // https://wiki.openstreetmap.org/wiki/Nominatim
+(function($) {
 
 var CPEnvKey = 'CPEnvJSON';
 var CPEnvLocationKey = 'CPEnvLocationJSON';
 var SourceIsHTML5 = 'HTML5-GeolocationAPI';
 var CPEnvironment = {}; // Namespace for CPEnvironment
-
-(function($) {
-
-// Geo-location
-function locationSourceIsHTML5() {
+	
+var getSourceOfLocation = function() {
 	var address = JSON.parse($.cookie(CPEnvLocationKey));
-	if(address != null && address.Source == SourceIsHTML5) {
-		return true;
+	if(address != null) {
+		switch (address.Source) {
+			case "HTML5-GeolocationAPI":
+				return "HTML5";
+			case "IPInfoDB":
+				return "IPInfoDB";
+			case "DebugToolbar":
+				return "DebugToolbar";
+			case "Default":
+				return "Default";
+			default:
+				return "Unknown";
+		}
 	} else {
-		return false;
+		return "NotSet";
 	}
 }
 
-if(!locationSourceIsHTML5() && navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(
-		function(position) {
-			$.getJSON( "http://nominatim.openstreetmap.org/reverse",
-					   { format: "json",
-				         lat: position.coords.latitude,
-				         lon: position.coords.longitude,
-				         zoom: 18,
-				         'accept-language': 'en' },
-				       function(json){
-				           var detailedAddress = json.address;
-				           
-				           var address = $.parseJSON($.cookie(CPEnvLocationKey));
-				           if(address == null) address = {};
-					
-				           address.lat = json.lat;
-				           address.lon = json.lon;
-				           
-				           address.Country = detailedAddress.country;
-				           address.Region = detailedAddress.state + ' ' + detailedAddress.region;
-				           address.City = detailedAddress.city;
-				           address.County = detailedAddress.county;
-				           address.Road = detailedAddress.road;
-				           address.PublicBuilding = detailedAddress.public_building;
-				           address.Postcode = detailedAddress.postcode;
-				           address.Source = SourceIsHTML5;
-					
-				           $.cookie(CPEnvLocationKey,JSON.stringify(address));
-				       }
-				     );
-	},
-	function(error) {
-	},
-	{timeout:10000});
+var getLocationWithHtml5 = function(failed) {
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+				function(position) {
+					$.getJSON( "http://nominatim.openstreetmap.org/reverse",
+							   { format: "json",
+						         lat: position.coords.latitude,
+						         lon: position.coords.longitude,
+						         zoom: 18,
+						         'accept-language': 'en' },
+						       function(json){
+						           var detailedAddress = json.address;
+						           
+						           var address = $.parseJSON($.cookie(CPEnvLocationKey));
+						           if(address == null) address = {};
+							
+						           address.lat = json.lat;
+						           address.lon = json.lon;
+						           
+						           address.Country = detailedAddress.country;
+						           address.Region = detailedAddress.state + ' ' + detailedAddress.region;
+						           address.City = detailedAddress.city;
+						           address.County = detailedAddress.county;
+						           address.Road = detailedAddress.road;
+						           address.PublicBuilding = detailedAddress.public_building;
+						           address.Postcode = detailedAddress.postcode;
+						           address.Source = SourceIsHTML5;
+							
+						           $.cookie(CPEnvLocationKey,JSON.stringify(address));
+						       }
+						     );
+			},
+			function(error) {
+				failed();
+			},
+			{timeout:10000});
+	} else {
+		failed();
+	}
 }
+
+var getLocationWithIPInfoDB = function(failed) {
+	// Get IPAddress of the client and call IPInfoDB
+	failed();
+}
+
+var doNothing = function() {}
+
+var getIPAddress = function() {
+	// Obtain IP Address from Cookie
+}
+
+$(function(){
+	if(!(getSourceOfLocation() == "HTML5" || getSourceOfLocation() == "DebugToolbar")){
+		getLocationWithHtml5(	getSourceOfLocation() == "IPInfoDB" ?
+								doNothing :
+								function(){ getLocationWithIPInfoDB(doNothing) });	
+	}
+});
 
 // APIs for getting / setting values from / to CPEnvironment
 // You can use these APIs if you want to get or set values from your javascript code.
