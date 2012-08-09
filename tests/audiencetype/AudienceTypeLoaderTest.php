a010c7cc04446d59d3f46d35403653df6f4659d0
Yuki Awano <y.a.unix@gmail.com>
2012-08-10 01:15:47 +0900

    MINOR Removed unused test case

--- tests/audiencetype/AudienceTypeLoaderTest.php
+++ tests/audiencetype/AudienceTypeLoaderTest.php
@@ -5,20 +5,6 @@
  */
 
 class AudienceTypeLoaderTest extends SapphireTest{
-	function testLoad(){
-		// You may need to rename mysite/_config/audiencetype.yml not to load the configuration.
-		
-		$audienceTypeLoader = new AudienceTypeLoader();
-		$result = $audienceTypeLoader->load();
-		$expected = array("InclusiveOR" => array(
-				"NewComer" => array("NewComer" => true),
-				"NewYorker" => array(
-						"Location" => "NewYork",
-						"Device" => "Linux")
-				));
-		$this->assertEquals($result,$expected);
-	}
-	
 	function testGetAudienceTypes() {
 		$data = array("InclusiveOR" => array(
 				"NewComer" => array("NewComer" => true),
